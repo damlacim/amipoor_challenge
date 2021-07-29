0d14ea995078a53d791779fcbce35e9cd0ea5a49
Damla Cim <cim.damla@gmail.com>
2021-07-29 15:57:59 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.black12,
+          appBar: AppBar(
+            backgroundColor: Colors.white10,
+            title: Center(
+              child: Text('I am poor'),
+            ),
+          ),
+          body: Center(
+            child: Image(image: AssetImage('images/dead.png')),
+          )),
+    ),
+  );
 }
